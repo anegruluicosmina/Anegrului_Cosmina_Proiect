c6f5c2fa8ad9194fefc0b0f383c765b668bf9980
MARIA-COSMINA ANEGRULUI <maria.anegrului@stud.ubbcluj.ro>
2021-10-21 19:50:13 +0300

    Lab4

--- Anegrului_Cosmina_Lab2/Controllers/HomeController.cs
+++ Anegrului_Cosmina_Lab2/Controllers/HomeController.cs
@@ -6,16 +6,18 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Anegrului_Cosmina_Lab2.Data;
+using Anegrului_Cosmina_Lab2.Models.LibraryViewModels;
 
 namespace Anegrului_Cosmina_Lab2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly LibraryContext _context;
+        public HomeController(LibraryContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -32,6 +34,18 @@ namespace Anegrului_Cosmina_Lab2.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public async Task<ActionResult> Statistics()
+        {
+            IQueryable<OrderGroup> data =
+            from order in _context.Orders
+            group order by order.OrderDate into dateGroup
+            select new OrderGroup()
+            {
+                OrderDate = dateGroup.Key,
+                BookCount = dateGroup.Count()
+            };
+            return View(await data.AsNoTracking().ToListAsync());
         }
     }
 }
